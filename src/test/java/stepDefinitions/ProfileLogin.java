d9bb1e4c15aa1a17d8135b0c7776a1e44296c0ef
santhoshtwinz <68216688+santhoshtwinz@users.noreply.github.com>
2025-03-23 01:11:05 +0530

    Update ProfileLogin.java

--- src/test/java/stepDefinitions/ProfileLogin.java
+++ src/test/java/stepDefinitions/ProfileLogin.java
@@ -11,13 +11,13 @@ import utilities.DriverConfig;
 public class ProfileLogin extends DriverConfig {
 
 	@Given("User navigates to Naukri webpage")
-	public void user_navigates_to_Naukri_webpage() {
-		// Call the setup method from the base class
-		setUp();
-		// Navigate to the specified URL
-		driver.get("https://www.naukri.com/");
+        public void user_navigates_to_Naukri_webpage() {
+        if (driver == null) {
+           throw new RuntimeException("WebDriver not initialized");
+        }
+         driver.get("https://www.naukri.com");
+        }
 
-	}
 
 	@When("^the user login and enters valid (.*) and (.*)$")
 	public void the_user_login_and_enters_valid_username_and_Automation(String username, String password) {
