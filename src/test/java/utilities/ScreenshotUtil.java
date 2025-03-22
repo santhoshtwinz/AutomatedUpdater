de6131e64db16603b01a37af0be4652f151d2be7
santhoshtwinz <68216688+santhoshtwinz@users.noreply.github.com>
2025-03-23 03:16:44 +0530

    Update ScreenshotUtil.java

--- src/test/java/utilities/ScreenshotUtil.java
+++ src/test/java/utilities/ScreenshotUtil.java
@@ -1,23 +1,41 @@
 package utilities;
 
-import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-
+import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ScreenshotUtil {
 
     public static void takeScreenshot(WebDriver driver, String screenshotName) {
-        // Take the screenshot
+        // Take screenshot and store it in a temporary file
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        // Define the directory where the screenshot will be saved
+        String directoryPath = "target/screenshots/";
+
+        // Create a timestamp for unique screenshot names
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+        String fileName = screenshotName + "_" + timestamp + ".png";
         
+        File destination = new File(directoryPath + fileName);
+
         try {
-            // Save it to the desired location
-            FileUtils.copyFile(screenshot, new File("./screenshots/" + screenshotName + ".png"));
+            // Create the screenshots folder if it doesn't exist
+            File dir = new File(directoryPath);
+            if (!dir.exists()) {
+                dir.mkdirs();  // Create the directory if it doesn't exist
+            }
+            
+            // Copy the screenshot to the specified location
+            FileUtils.copyFile(screenshot, destination);
+            System.out.println("Screenshot saved to: " + destination.getAbsolutePath());
         } catch (IOException e) {
+            System.out.println("Error saving screenshot: " + e.getMessage());
             e.printStackTrace();
         }
     }
