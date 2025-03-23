b22597d8d3308e6380da4a2b0cce77340bcd76c8
santhoshtwinz <68216688+santhoshtwinz@users.noreply.github.com>
2025-03-23 15:01:23 +0530

    Update ProfileLogin.java

--- src/test/java/stepDefinitions/ProfileLogin.java
+++ src/test/java/stepDefinitions/ProfileLogin.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 
 import methods.LoginPage;
@@ -26,11 +29,14 @@ public class ProfileLogin extends DriverConfig {
             // Find the search bar using its name attribute and type "naukri"
            WebElement searchBox = driver.findElement(By.name("q"));
            searchBox.sendKeys("naukri");
-           searchBox.sendKeys(Keys.RETURN);
+           searchBox.sendKeys(Keys.ENTER);   // Equivalent, works the same
 	    // Press the Enter key to start the search
            System.out.println("searching for naukri");
+           WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));  // Wait for up to 8 seconds
 	   ScreenshotUtil.takeScreenshot(driver, "search_homepage"); // Take screenshot of homepage
             // Wait for the results to load and click on the first search result link
+	//   WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[contains(text(), 'Naukri.com: Jobs')]")));  // Wait until the element is visible
+
     //        WebElement firstLink = driver.findElement(By.xpath("//h3[contains(text(), 'Naukri.com: Jobs')]"));
     //        firstLink.click();  // Click on the first result
     //        String currentUrl = driver.getCurrentUrl();
